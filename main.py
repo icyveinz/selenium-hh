fcaffa46c78f005d212461c1158618be78d93af6
Egor Ivanov <rust_lang_lover@proton.me>
2025-02-01 22:09:35 +0300

    // now works only with unread messages, and if it can't find any dec

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from time import sleep
+
+from selenium.webdriver import ActionChains, Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,13 +11,14 @@ print(driver.title)
 
 wait = WebDriverWait(driver, 10)
 
+sleep(5)
+filter_div_locator = (By.CSS_SELECTOR, 'div.BUagpy6___filter-item')
+filter_div = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(filter_div_locator))
+filter_div.click()
+
 # Loop to process all chats with the "Отказ" message
 while True:
     try:
-        filter_div_locator = (By.CSS_SELECTOR, 'div.BUagpy6___filter-item')
-        filter_div = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(filter_div_locator))
-        filter_div.click()
-
         # Locate all chat cells
         chat_cells_locator = (By.CLASS_NAME, 'oWKN7Ez___chat-cell')
         chat_cells = wait.until(EC.presence_of_all_elements_located(chat_cells_locator))
@@ -55,8 +59,19 @@ while True:
                 print(f"No 'Отказ' message in this chat. Error: {inner_e}")
 
         # After processing all chat cells, refresh to get any new chats if needed
-        driver.refresh()
-        print("Refreshing to get new chat data.")
+        # Locate the scrollable element
+        scrollable_element_locator = (By.CLASS_NAME, 'VKJD3HM___chats')
+
+        # Wait until the element is present
+        scrollable_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located(scrollable_element_locator))
+
+        # Use ActionChains to scroll down
+        actions = ActionChains(driver)
+        actions.move_to_element(scrollable_element).click().send_keys(Keys.END).perform()
+
+        print("Scrolled down using ActionChains!")
+        # driver.refresh()
+        # print("Refreshing to get new chat data.")
 
     except Exception as e:
         print(f"Error during chat processing: {e}")
