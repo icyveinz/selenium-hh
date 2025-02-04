4dbbfc62a27b4350fc04a107715864954dfcd558
Egor Ivanov <rust_lang_lover@proton.me>
2025-02-04 21:50:27 +0300

    // updated

--- enitites/chat.py
+++ enitites/chat.py
@@ -1,3 +1,5 @@
+from selenium.common.exceptions import NoSuchElementException
+
 class Chat:
     def __init__(self, element):
         self.element = element
@@ -6,7 +8,10 @@ class Chat:
         try:
             self.element.find_element("xpath", './/div[contains(@class, "mnIHQ0E___last-message AkDJdek___last-message-color_red") and text()="Отказ"]')
             return True
-        except:
+        except NoSuchElementException:
+            return False
+        except Exception as e:
+            print(f"Unexpected error: {e}")
             return False
 
     def click(self):
