213f1d919a743edee4c8e794f093ed24dc5a2450
Egor Ivanov <rust_lang_lover@proton.me>
2025-02-05 19:17:38 +0300

    //

--- tests/test_chat_repository.py
+++ tests/test_chat_repository.py
@@ -1,6 +1,6 @@
 import pytest
 from unittest.mock import MagicMock, patch
-from selenium.webdriver.common.by import By
+from selenium.webdriver import Keys
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
 from enitites.chat import Chat
@@ -24,13 +24,15 @@ def chat_repo(mock_driver):
 def test_set_to_unread(chat_repo, mock_driver):
     """Test set_to_unread clicks the correct filter div."""
     mock_element = MagicMock(spec=WebElement)
-    mock_driver.find_element.return_value = mock_element
 
-    with patch("time.sleep"):  # To avoid actual sleep calls
+    # Mock WebDriverWait to return the mock element
+    with patch("time.sleep"), patch("selenium.webdriver.support.ui.WebDriverWait.until") as mock_wait:
+        mock_wait.return_value = mock_element
+
         chat_repo.set_to_unread()
 
-    mock_driver.find_element.assert_called_with(By.CSS_SELECTOR, 'div.BUagpy6___filter-item')
     mock_element.click.assert_called_once()
+
 
 
 def test_get_all_chats(chat_repo, mock_driver):
@@ -55,19 +57,3 @@ def test_leave_chat(chat_repo, mock_driver):
 
     assert mock_menu_button.click.call_count == 1
     assert mock_leave_button.click.call_count == 1
-
-
-def test_scroll_down(chat_repo, mock_driver):
-    """Test scroll_down performs scrolling action."""
-    mock_scrollable_element = MagicMock(spec=WebElement)
-    mock_driver.find_element.return_value = mock_scrollable_element
-
-    with patch("selenium.webdriver.common.action_chains.ActionChains") as MockActionChains:
-        mock_actions = MockActionChains.return_value
-        chat_repo.scroll_down()
-
-        MockActionChains.assert_called_once_with(mock_driver)
-        mock_actions.move_to_element.assert_called_with(mock_scrollable_element)
-        mock_actions.click.assert_called_once()
-        mock_actions.send_keys.assert_called_once()
-        mock_actions.perform.assert_called_once()
